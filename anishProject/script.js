63099b980d729b372b38cc84ce534fec9cb34bf9
mrkool <ak1380042@gmail.com>
2023-04-02 20:39:08 +0530

    AnishProject modified

--- anishProject/script.js
+++ anishProject/script.js
@@ -1,3 +1,4 @@
+
 const form = document.getElementById('location-form');
 const mapDiv = document.getElementById('map');
 const heritagePlacesDiv = document.getElementById('heritage-places');
@@ -9,26 +10,26 @@ form.addEventListener('submit', (event) => {
 });
 
 function searchLocation(location) {
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '2b613986a7mshf5d74bb567bfb52p102307jsn1bb787a74b6d',
-      'X-RapidAPI-Host': 'address-from-to-latitude-longitude.p.rapidapi.com'
-    }
-  };
 
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '2b613986a7mshf5d74bb567bfb52p102307jsn1bb787a74b6d',
+            'X-RapidAPI-Host': 'address-from-to-latitude-longitude.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://address-from-to-latitude-longitude.p.rapidapi.com/geolocationapi?address=Eiffel%20Tower', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
   // Use a geocoding API to convert the location to latitude and longitude
   // For example, you could use the Google Maps Geocoding API:
   // https://developers.google.com/maps/documentation/geocoding/start
-  fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyBBHWfC_ITH6XVxpcQfxwSN1CmXCAQPPEw`)
-    .then(response => response.json())
-    .then(response => {
-      const latitude = response.results[0].geometry.location.lat;
-      const longitude = response.results[0].geometry.location.lng;
-      displayMap(latitude, longitude);
-      displayHeritagePlaces(latitude, longitude);
-    })
-    .catch(err => console.error(err));
+  //const latitude = https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY; 
+  //const longitude =https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY ; 
+  displayMap(latitude, longitude);
+  displayHeritagePlaces(latitude, longitude);
 }
 
 function displayMap(latitude, longitude) {
@@ -45,39 +46,4 @@ function displayHeritagePlaces(latitude, longitude) {
   // Use a heritage places API to get a list of heritage places near the specified latitude and longitude
   // For example, you could use the UNESCO World Heritage API:
   // https://whc.unesco.org/en/api/
-  const options = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    }
-  };
-
-  fetch(`https://whc.unesco.org/en/list/json/?lat=${latitude}&lon=${longitude}&radius=50&max=10`, options)
-    .then(response => response.json())
-    .then(response => {
-      const heritagePlaces = response.map(place => ({
-        name: place.name_en,
-        description: place.short_description_en,
-        image: place.image_url,
-        location: {
-          latitude: place.latitude,
-          longitude: place.longitude
-        }
-      }));
-      renderHeritagePlaces(heritagePlaces);
-    })
-    .catch(err => console.error(err));
-}
-
-function renderHeritagePlaces(heritagePlaces) {
-  const heritagePlacesHTML = heritagePlaces.map(place => `
-    <div class="heritage-place">
-      <h2>${place.name}</h2>
-      <img src="${place.image}" alt="${place.name}">
-      <p>${place.description}</p>
-      <p>Latitude: ${place.location.latitude}</p>
-      <p>Longitude: ${place.location.longitude}</p>
-    </div>
-  `).join('');
-  heritagePlacesDiv.innerHTML = heritagePlacesHTML;
-}
+  }
